8844eb5567ba5eaf2d86fb84988c4397cd4fed78
Giles Davies <gidavies@github.com>
2023-05-22 09:45:43 +0000

    demo ready

---
+++ house/sketch.js
@@ -0,0 +1,17 @@
+/*
+Draw a house
+with a blue door
+with a doorknob
+and a brown roof
+and two blue windows
+and a black gutter
+and a red chimney
+*/
+
+function setup(){
+    createCanvas(400,400);
+  }
+  
+  function draw(){
+  
+  }
---
+++ src/test/java/com/github/demo/service/BookUtilsTest.java
@@ -0,0 +1,12 @@
+package com.github.demo.service;
+
+import com.github.demo.model.Book;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+public class BookUtilsTest {
+    
+}
